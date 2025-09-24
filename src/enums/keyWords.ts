a33ea2d87f6cc5053b3e3bed9c98cec4a89a3f49
Juan Alvarez <juanalvarezzz.dev@gmail.com>
2025-09-24 11:40:12 -0500

    Add Keywords enum for standardized string constants in the application

---
+++ src/enums/keyWords.ts
@@ -0,0 +1,13 @@
+export enum Keywords {
+    Pantalla = 'Pantalla',
+    Mostramos = 'Mostramos',
+    Opciones = 'Opciones',
+    SiEliges = 'Si eliges',
+    IrAPantalla = 'Ir a pantalla',
+    Salir = 'Salir',
+    Cancelar = 'Cancelar',
+    Sí = 'Sí',
+    No = 'No',
+    Imagen = 'Imagen',
+    Formulario = '[FORMULARIO]',
+}
